b52a42b20182f72377d72d8c99b4a799792ad89e
laura-bvc <156967330+laura-bvc@users.noreply.github.com>
2024-03-21 14:18:43 -0600

    Draft 1 for JS

---
+++ script.js
@@ -0,0 +1,84 @@
+
+/**
+* @name: Project
+* @Course Code: SODV1201
+* @class: Software Development Diploma program.
+* @author: Laurainda Fan & Joey Pinaroc
+**/
+
+// users: include owner & coworker (as array)
+// ->owner: user name, password, "type=owner", workspace ID array
+// ->coworker: user name, password, "type=coworker", saved search, booking
+// workspace: ID, owner ID, title, location, description, capacity, amenities, avaliability, bookings
+
+// booking: workspace ID, coworker ID, date
+
+// Search - match title, location, description, amenities by individual space delimited string
+
+
+var all_workspace = [
+	{
+		"ID": 1,
+		"owner": 1,
+		"title": "Workspace 1",
+		"location": "Downtown",
+		"desc": "Great for board meeting",
+		"capacity": 15,
+		"amenities": "WiFi, Underground parking and street parking, Elevator",
+		"avaliability": [1], //0=Sun, 1=Mon,..., 6=Sat
+		"bookings": []
+	},
+	{
+		"ID": 2,
+		"owner": 2,
+		"title": "Workspace 2",
+		"location": "Downtown",
+		"desc": "Office space",
+		"capacity": 3,
+		"amenities": "Wifi",
+		"avaliability": [1,2,3,4,5], //0=Sun, 1=Mon,..., 6=Sat
+		"bookings": []
+	},
+	{
+		"ID": 3,
+		"owner": 2,
+		"title": "Workspace 3",
+		"location": "Beltline",
+		"desc": "Meeting room",
+		"capacity": 6,
+		"amenities": "Street parking",
+		"avaliability": [0,2,5], //0=Sun, 1=Mon,..., 6=Sat
+		"bookings": []
+	}];
+	
+var all_users;
+
+$(document).ready(function(){
+
+// search.html 
+// ************* redirect to login with search terms *************
+
+	if ($(".button_to_login").length) {
+		$(".button_to_login").click(function(){
+			//********save search terms
+			window.location.href='login.html?search=' + ($("#div_search").text()).slice(14);
+		});	
+	}
+
+// coworker_search.html
+// ************* redirect to coworker_booking with property ID *************
+
+	else if ($(".button_to_book").length) {
+		$(".button_to_book").click(function(){
+			//******** pass property ID
+			window.location.href='coworker_booking.html?book=' + $(this).closest('div').attr('id').slice(2);
+		});	
+	}
+	
+// coworker_booking.html
+// ************* check date valid (correct avaliability, not yet booked), 
+//        -> redirect to coworker_search with confirmation message. 
+//        -> Save booking details *************	
+
+
+});	
